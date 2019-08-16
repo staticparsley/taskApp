b694ce55a2e9a44821a8d1ac60779f535c6bf531
Ruben Marin <staticparsley92@gmail.com>
2019-08-16 18:07:56 -0400

    some cleanup

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,45 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)){
-                throw new Error('not a valid email')
-            }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) throw new Error('age must be positive');
-        }
-    }
-});
-
-const me = new User({
-    name: 'Ho',
-    age: 10,
-    email: 'realEmail@email.com'
-});
-
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!',error);
-})
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('not a valid email')
+            }
+        },
+    },
+    password: {
+        type: String,
+        requried: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) throw new Error('age must be positive');
+        }
+    }
+});
+
+module.exports = User;
