11280d00db8309207fed908e4d7b6e93b83091be
Ruben Marin <staticparsley92@gmail.com>
2019-08-18 22:26:20 -0400

    some small changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -35,6 +35,17 @@ const User = mongoose.model('User', {
             if (value < 0) throw new Error('age must be positive');
         }
     }
+
 });
+
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    console.log('just before saving');
+    next();
+})
+
+const User = mongoose.model('User', UserSchema);
+
+
 
 module.exports = User;
