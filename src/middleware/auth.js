42e92e30ae78e8d5fcde1f30994af861ae68bc29
Ruben Marin <staticparsley92@gmail.com>
2019-08-19 00:28:52 -0400

    added ability to logout

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,13 +3,14 @@ const User = require('../models/user');
 
 const auth = async (req,res,next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer', '');
+        const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'thisismynewcourse');
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
         if(!user) {
             throw new Error();
         }
 
+        req.token = token;
         req.user = user;
         next();
 
