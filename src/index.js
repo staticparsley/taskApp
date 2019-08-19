f19cb39ab245e5a138530f5797941015688b84f8
Ruben Marin <staticparsley92@gmail.com>
2019-08-18 23:44:28 -0400

    added some authentication for user

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
-app.use(express.json());
 
+
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
