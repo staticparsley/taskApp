7943c83f8342387a85dfa64b04ae628463da0d94
Ruben Marin <staticparsley92@gmail.com>
2019-08-19 01:29:41 -0400

    cascading delete

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('../models/task');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -98,6 +99,12 @@ UserSchema.pre('save', async function(next) {
         user.password = await bcrypt.hash(user.password,8);
     }
 
+    next();
+});
+
+UserSchema.pre('remove', async function(next) {
+    const user = this;
+    await Task.deleteMany({owner: user._id});
     next();
 })
 
