8a3ff43c822e515c6b1844af422301d3aef461cb
Ruben Marin <staticparsley92@gmail.com>
2019-08-18 23:22:06 -0400

    added jwt to login and create user

--- src/index.js
+++ src/index.js
@@ -14,17 +14,14 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const password = 'Red1234';
-    const hashedPassword = await bcrypt.hash(password, 8);
+    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'});
+    console.log(token);
 
-    console.log(password);
-    console.log(hashedPassword);
-
-    const isMatch = await bcrypt.compare('red1234', hashedPassword);
-    console.log(isMatch);
+    const data = jwt.verify(token,'thisismynewcourse');
+    console.log(data);
 }
 
 myFunction();
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -36,9 +37,26 @@ const UserSchema = new mongoose.Schema({
         validate(value) {
             if (value < 0) throw new Error('age must be positive');
         }
-    }
+    },
+    tokens: [{
+        token : {
+            type: String,
+            required: true
+        }
+    }]
 
 });
+
+UserSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+
+    return token;
+
+}
 
 UserSchema.statics.findByCredentials = async (email,password) => {
     const user = await User.findOne({email: email });
